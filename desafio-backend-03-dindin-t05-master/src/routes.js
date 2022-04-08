dd7c226214bdb2972fe5081bed6050cbb3da5b6c
Élder Müller <elder.muller@gmail.com>
2022-04-08 16:43:40 -0300

    feature login implementada

--- desafio-backend-03-dindin-t05-master/src/routes.js
+++ desafio-backend-03-dindin-t05-master/src/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { registerUser } = require('./controllers/users');
+const login = require('./controllers/login');
 
 const routes = express();
 
 routes.post('/usuario', registerUser);
+
+routes.post('/login', login);
 
 module.exports = routes;
