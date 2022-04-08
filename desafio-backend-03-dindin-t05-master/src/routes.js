42d9f7f2c0568c135c5676c8c92cdd9bb2ef06bf
Élder Müller <elder.muller@gmail.com>
2022-04-08 17:04:58 -0300

    loginVerify implementado

--- desafio-backend-03-dindin-t05-master/src/routes.js
+++ desafio-backend-03-dindin-t05-master/src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { registerUser } = require('./controllers/users');
 const login = require('./controllers/login');
+const loginVerify = require('./filters/loginVerify');
 
 const routes = express();
 
 routes.post('/usuario', registerUser);
 
 routes.post('/login', login);
+
+routes.use(loginVerify);
 
 module.exports = routes;
